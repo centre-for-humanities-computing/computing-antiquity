858837a1632ad94f9a2626fd701130794e933e6b
Márton Kardos <power.up1163@gmail.com>
2023-03-06 14:29:46 +0100

    updated dependencies

--- src/scripts/spacy_process_corpus.sh
+++ src/scripts/spacy_process_corpus.sh
@@ -4,7 +4,9 @@ python3 -m venv environments/processing
 source environments/processing/bin/activate
 pip install "pandas>=1.5.0,<1.6.0"
 pip install "tqdm>=4.65.0,<4.66.0"
-pip install "spacy[cuda-autodetect]>=3.5.0,<3.6.0"
+pip install torch==1.8.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+pip install spacy[cuda101]
+pip install --upgrade torch
 pip install "wandb>=0.13.0,<0.14.0"
 pip install "plotly>=5.13.0,<5.14.0"
 
