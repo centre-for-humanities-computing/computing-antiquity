07db1686504646ce800c038eda2facb87ef060a4
Márton Kardos <power.up1163@gmail.com>
2023-03-06 14:42:25 +0100

    changed dependency order

--- src/scripts/spacy_process_corpus.sh
+++ src/scripts/spacy_process_corpus.sh
@@ -5,9 +5,9 @@ python3 -m venv environments/processing
 source environments/processing/bin/activate
 pip install torch==1.8.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 pip install spacy[cuda101]
+pip install --upgrade torch
 pip install "pandas>=1.5.0,<1.6.0"
 pip install "tqdm>=4.65.0,<4.66.0"
-pip install --upgrade torch
 pip install "wandb>=0.13.0,<0.14.0"
 pip install "plotly>=5.13.0,<5.14.0"
 
