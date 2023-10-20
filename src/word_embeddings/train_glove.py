580676f5dd9e494a9cb5c6416caab6ac2b4f4cbc
Márton Kardos <power.up1163@gmail.com>
2023-10-20 14:58:43 +0200

    Added Jacob's new queries to word embedding training

--- src/word_embeddings/train_glove.py
+++ src/word_embeddings/train_glove.py
@@ -5,10 +5,19 @@ import pandas as pd
 from gensim.utils import deaccent
 from glovpy import GloVe
 
-AUTHORS = ["Plutarch", "Philo Judaeus"]
 DATA_PATH = "dat/greek/cleaned_data/lemmatized_without_stopwords.csv"
 OUT_PATH = "dat/greek/models/glove/"
 SHEET_URL = "https://docs.google.com/spreadsheets/d/15WIzk2aV3vCQLnDihdnNCLxMbDmJZiZKmuiM_xRKbwk/edit#gid=282554525"
+QUERIES = [
+    ("Author", "Plutarch"),
+    ("Author", "Philo Judaeus"),
+    ("Group", "Greek Novels"),
+    ("Group", "LXX Historiographies"),
+    ("Group", "LXX Propheteia"),
+    ("Group", "LXX Poetry/Wisdom"),
+    ("Group", "Jewish Novels"),
+    ("Group", "NT Narratives"),
+]
 
 
 def fetch_metadata(url: str) -> pd.DataFrame:
@@ -25,20 +34,21 @@ def main():
     print("Loading metadata.")
     metadata = fetch_metadata(SHEET_URL)
     data = data.merge(metadata, on="document_id")
-    data = data[data.author.isin(AUTHORS)]
     print("Preprocessing corpus.")
+    data.text = data.text.fillna("")
     data.text = data.text.str.lower()
     data.text = data.text.map(deaccent)
     data.text = data.text.map(lambda s: s.split())
     out_dir = Path(OUT_PATH)
     out_dir.mkdir(exist_ok=True, parents=True)
-    for author in AUTHORS:
-        print(f"Training model for {author}...")
-        corpus = list(data.text[data.author == author])
+    for column, value in QUERIES:
+        print(f"Training model for {column} - {value}...")
+        corpus = list(data.text[data[column.lower()] == value])
         model = GloVe(vector_size=50, window_size=15, iter=25)
         model.train(corpus)
         print("Saving.")
-        out_filename = f"Author - {author} - Glove.gensim"
+        value_name = value.replace("/", "-")
+        out_filename = f"{column} - {value_name} - Glove.gensim"
         model.wv.save(str(out_dir.joinpath(out_filename)))
     print("DONE")
 
